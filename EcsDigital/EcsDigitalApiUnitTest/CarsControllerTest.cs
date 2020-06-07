489ef9649121eac668e422a3896e88d805073f58
Oshry Horn <OHorn@travelrepublic.co.uk>
2020-06-07 14:14:27 +0100

    #2 Initial solution, and API and Test projects

---
+++ EcsDigital/EcsDigitalApiUnitTest/CarsControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EcsDigitalApiUnitTest
+{
+    public class CarsControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
